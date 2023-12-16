f4908335bb6f16354c266382ae782b1e7d2f0074
✧*̥˚ Gabriela *̥˚✧ <144128006+gabibritss@users.noreply.github.com>
2023-12-15 23:21:48 -0300

    questão 4.c

--- questão 4.c
+++ questão 4.c
@@ -1,75 +1,88 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct contaBancaria {
+    int Conta;
+    char nome[50];
+    float saldo;
+    char tipoConta[20];
+};
 
-int main()
-{
 
-    int i;
-    int numeros[5];
-    int par = 0;
-    int impar = 0;
-    int somaImpar = 0;
-    int positivos = 0;
-    int negativos = 0;
-    float mediaPares = 0;
-    int somaPares = 0;
-    float mediaTodos = 0;
-    int somaTodos = 0;
-    int maiorN = 0;
-    int menorN = 999;
-    int quantidade = 0;
-
-    for (i = 0; i < 5; i++)
-    {
-        printf("Digite o %dº número: ", i + 1);
-        scanf("%d", &numeros[i]);
-
-        quantidade++;
-        somaTodos += numeros[i];
-        //parsoma e imparsoma//
-        if (numeros[i] % 2 == 0)
-        {
-            par++;
-            somaPares += numeros[i];
-        }
-        else
-        {
-            impar++;
-            somaImpar += numeros[i];
-        }
-       //positivo e negativo//
-        if (numeros[i] > 0)
-        {
-            positivos++;
-        }
-        else
-        {
-            negativos++;
-        }
-        // maior e menor//
-        maiorN = numeros[i] > maiorN ? numeros[i] : maiorN;
-        menorN = numeros[i] < menorN ? numeros[i] : menorN;
+void depositar(struct contaBancaria *conta, float valor) {
+    if (valor > 0) {
+        conta->saldo += valor;
+        printf("\nDepósito de R$%.2f realizado com sucesso.", valor);
+    } else {
+        printf("\nDepósito não foi realizado, valor inválido.");
     }
-       //media par e soma todos//
-    if (par > 0)
-    {
-        mediaPares = somaPares / (float) par;
+}
+
+
+void sacar(struct contaBancaria *conta, float valor) {
+    if (valor > 0 && valor <= conta->saldo) {
+        conta->saldo -= valor;
+        printf("Saque de R$%.2f realizado com sucesso.\n", valor);
+    } else {
+        printf("\nValor inválido ou saldo insuficiente. O saque não foi realizado.");
     }
+}
 
-    mediaTodos = somaTodos / (float) i;
 
-    for (i = 0; i < 5; i++)
-    {
-        printf("%dº número: %d\n", i + 1, numeros[i]);
-    }
+void imprimirSaldo(struct contaBancaria conta) {
+    printf("Saldo atual da conta: R$%.2f\n", conta.saldo);
+}
 
-    printf("\nQuantidade de números ímpares: %d\n", impar);
-    printf("Quantidade de números negativos: %d\n", negativos);
-    printf("Maior número: %d\n", maiorN);
-    printf("Menor número: %d\n", menorN); 
-    printf("Média de números pares: %.1f\n", mediaPares);
-    printf("Média dos números inseridos: %.1f\n", mediaTodos);
+int main() {
+    
+    struct contaBancaria minhaConta = {12345, "Gabis", 10.000, "Corrente"};
+
+    int opcao;
+    float valor;
+
+    do {
+    
+        printf("\n--------Menu--------\n");
+        printf("\n1- Depositar dinheiro.");
+        printf("\n2- Sacar dinheiro.");
+        printf("\n3- Imprimir saldo.");
+        printf("\n4- Sair do programa.");
+
+        
+        printf("\nEscolha uma opção: ");
+        scanf("%d", &opcao);
+
+        switch (opcao) {
+            case 1:
+                
+                printf("\nDigite o valor a ser depositado: ");
+                scanf("%f", &valor);
+                depositar(&minhaConta, valor);
+                break;
+
+            case 2:
+                
+                printf("\nDigite o valor a ser sacado: ");
+                scanf("%f", &valor);
+                sacar(&minhaConta, valor);
+                break;
+
+            case 3:
+                
+                imprimirSaldo(minhaConta);
+                break;
+
+            case 4:
+                
+                printf("\nSaindo do programa.");
+                break;
+
+            default:
+                
+                printf("Opção inválida. Tente novamente....\n");
+        }
+
+    } while (opcao != 4);
 
     return 0;
 }
